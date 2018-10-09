2329111f47120ddf425da0bfe10ef65f900eed41
Timothy Wang <timothywangdev@gmail.com>
2018-10-09 17:26:44 +0000

    Resolve #33 Integrate ProjectDetailComponent into ProjectComponent

--- src/Project/ProjectComponent.jsx
+++ src/Project/ProjectComponent.jsx
@@ -18,6 +18,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Toolbar from '@material-ui/core/Toolbar'
+import ProjectDetail from './ProjectDetailComponent'
 import logo from '../logo.svg'
 var numeral = require('numeral')
 
@@ -28,7 +29,86 @@ class ProjectComponent extends Component {
     super()
     this.milestone = new Milestone()
     this.state = {
-      ready: false
+      tabValue: 0,
+      ready: false,
+      tokenInfo: {
+        forSale: '18000000'
+      },
+      projectData: {
+        projectName: 'Blockcloud',
+        logo: '/mock_logo.png',
+        shortDescription: 'A Blockchain-based Advanced TCP/IP Architecture Providing Constant Connectivity for Dynamic Networks',
+        video: 'https://www.youtube.com/watch?v=64kQLRZeE_E',
+        description: 'While BigData has traditionally been available only to big companies, Blockcloud.io lowers the barrier for entry and expands our potential client base to include small, medium and large businesses around the globe as well as ICOs seeking data for their new ventures.',
+        corporationInfo: {
+          location: {
+            country: 'CA',
+            city: 'Toronto'
+          },
+          team: {
+            teamSize: 6,
+            members: [
+              {
+                name: 'Raul Romero',
+                title: 'CEO',
+                link: 'www.google.com'
+              }, {
+                name: 'Zi Wen Zhang',
+                title: 'CTO',
+                link: 'www.google.com'
+              }, {
+                name: 'Lawrence Duerson',
+                title: 'COO',
+                link: 'www.google.com'
+              }, {
+                name: 'Raul Romero',
+                title: 'CEO',
+                link: 'www.google.com'
+              }, {
+                name: 'Zi Wen Zhang',
+                title: 'CTO',
+                link: 'www.google.com'
+              }, {
+                name: 'Lawrence Duerson',
+                title: 'COO',
+                link: 'www.google.com'
+              }
+            ]
+          }
+        },
+        category: 'Artificial Intelligence',
+        website: 'www.google.com',
+        whitepaper: 'www.google.com',
+        token: {
+          symbol: 'BLOC',
+          price: '10',
+          platform: 'Ethereum',
+          accept: ['ETH', 'BTC'],
+          KYC: true,
+          cantParticipate: ['CN', 'US']
+        },
+        socialLinks: [
+          {
+            type: 'telegram',
+            link: 'www.google.com'
+          }, {
+            type: 'github',
+            link: 'www.google.com'
+          }, {
+            type: 'reddit',
+            link: 'www.google.com'
+          }, {
+            type: 'twitter',
+            link: 'www.google.com'
+          }, {
+            type: 'facebook',
+            link: 'www.google.com'
+          }, {
+            type: 'slack',
+            link: 'www.google.com'
+          }
+        ]
+      }
     }
   }
 
@@ -100,6 +180,10 @@ class ProjectComponent extends Component {
     return rv
   }
 
+  handleTabChange = (event, value) => {
+    this.setState({tabValue: value })
+  }
+
   renderMilestone = (m) => {
     let { classes } = this.props
     let content = m.content
@@ -132,8 +216,32 @@ class ProjectComponent extends Component {
     )
   }
 
+  renderTimeline = () => {
+    let { classes, projectData } = this.props
+    return (
+      <Grid item>
+        <VerticalTimeline animate={false}>
+          { projectData.milestones.map((m) => this.renderMilestone(m))}
+        </VerticalTimeline>
+      </Grid>
+    )
+  }
+
+  renderDetail = () => {
+    let { projectData, tokenInfo } = this.state
+    return (
+      <Grid item>
+        <Grid container direction='column' alignItems='center'>
+          <Grid item lg={6}>
+            <ProjectDetail projectData={projectData} tokenInfo={tokenInfo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+
   render () {
-    let value = 0
+    let { tabValue } = this.state
     let { classes, projectData } = this.props
 
     if (!projectData) {
@@ -200,18 +308,14 @@ class ProjectComponent extends Component {
               </Grid>
               <Grid item>
                 <AppBar className={classes.tabsBar} position='static'>
-                  <Tabs indicatorColor='primary' value={value} onChange={this.handleChange} centered >
+                  <Tabs indicatorColor='primary' value={tabValue} onChange={this.handleTabChange} centered >
                     <Tab label='MILESTONES' />
                     <Tab label='INFO' />
-                    <Tab label='TEAM' />
                   </Tabs>
                 </AppBar>
               </Grid>
-            </Grid>
-            <Grid item>
-              <VerticalTimeline animate={false}>
-                { projectData.milestones.map((m) => this.renderMilestone(m))}
-              </VerticalTimeline>
+              { tabValue === 0 && this.renderTimeline() }
+              { tabValue === 1 && this.renderDetail() }
             </Grid>
           </Grid>
         </Grid>
